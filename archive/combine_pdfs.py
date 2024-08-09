40acb375c01ad2dfd8aabe6a03b0bda84dc18fe7
Vadim Geshiktor <vgeshiktor@gmail.com>
2024-08-09 22:23:05 +0300

    add old files

--- archive/combine_pdfs.py
+++ archive/combine_pdfs.py
@@ -4,11 +4,17 @@ from pathlib import Path
 import PyPDF2
 
 # specify the directory containing the PDF files to be combined
-pdf_dir = r'C:\Users\innas\OneDrive\Рабочий стол\workers\Victoria Brosilovski'
+pdf_dir = r"C:\Users\innas\OneDrive\Рабочий стол\workers\Victoria Brosilovski"
 
 # get a list of the PDF files in the directory
 # pdf_files = [filename for filename in os.listdir(pdf_dir) if filename.endswith('.pdf')]
-pdf_files = [Path(rf'{pdf_dir}\{filename}') for filename in ('viktoria-brosilovski-161-2023-1.pdf', 'viktoria-brosilovski-161-2023-2.pdf')]
+pdf_files = [
+    Path(rf"{pdf_dir}\{filename}")
+    for filename in (
+        "viktoria-brosilovski-161-2023-1.pdf",
+        "viktoria-brosilovski-161-2023-2.pdf",
+    )
+]
 
 # create a new PDF file to hold the merged PDFs
 output_pdf = PyPDF2.PdfMerger()
@@ -16,11 +22,13 @@ output_pdf = PyPDF2.PdfMerger()
 # add each PDF file to the output PDF
 for filename in pdf_files:
     filepath = os.path.join(pdf_dir, filename)
-    pdf_file = open(filepath, 'rb')
+    pdf_file = open(filepath, "rb")
     output_pdf.append(pdf_file)
 
 # write the output PDF to a file
-output_filepath = os.path.join(pdf_dir, 'viktoria-brosilovski-161-2023-signed.pdf')
+output_filepath = os.path.join(
+    pdf_dir, "viktoria-brosilovski-161-2023-signed.pdf"
+)
 output_pdf.write(output_filepath)
 
 # close all the open PDF files
